bf9c8ab57bb48ee02ae39b9c843173c060224ca5
Vivers0 <vivers.savva@gmail.com>
2022-07-24 23:53:25 +0300

    [+] teacher request added in switrcher

--- requests/register.request.js
+++ requests/register.request.js
@@ -1,14 +1,29 @@
 import { link } from '../env.local.json';
 
 export const registerRequest = async (data) => {
-    return fetch(`${link}/student/create`,
-    {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data),
-    })
-    .then(r => r.json())
+    switch (data.type) {
+        case 'student':
+            return fetch(`${link}/student/create`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                })
+                .then(r => r.json())
+        case 'teacher':
+            return fetch(`${link}/teacher/create`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data),
+                })
+                .then(r => r.json())
+    }
+
 }
